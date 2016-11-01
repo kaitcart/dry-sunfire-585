cdfc95fda26c975e0b42f66387d194e5e55493cb
Jay Wallace <jaycwallace@gmail.com>
2016-11-01 13:23:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# riak-example
+make sure to change out the username
---
+++ scripts/riak.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#download riak ts package
+sudo curl -LO http://s3.amazonaws.com/downloads.basho.com/riak_ts/1.4/1.4.0/rhel/6/riak-ts-1.4.0-1.el6.x86_64.rpm
+
+#install package
+sudo yum install -y riak-ts-1.4.0-1.el6.x86_64.rpm
+
+#start the node
+sudo riak start
